fa3170f14337226b671ecab8b35ef34c71e37386
Yaroslav <yaroslav@Yaroslavs-MacBook-Pro.local>
2018-09-09 10:14:34 -0400

    update

--- pipeline/contactsToCytoscape.py
+++ pipeline/contactsToCytoscape.py
@@ -18,8 +18,8 @@
 #!/usr/bin/env python 3
 import re
 
-#PATH = "../data/Interfaces/"
-PATH = "/net/pan1/interactomes/pipeline/Interactome/Workflow/Interfaces/"
+PATH = "../data/Interfaces/"
+#PATH = "/net/pan1/interactomes/pipeline/Interactome/Workflow/Interfaces/"
 CHAIN_FILE = "text.tsv"
 PDB_LIST = "pdbList.txt"
 
